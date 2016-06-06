875ad6b3e6e696bf0d3a81a716ba668d3e7b16ca
Arron Atchison <arron.atchison@gmail.com>
2016-06-06 10:42:12 -0500

    added trailing '/ ' to mimic version upload path

--- publish/publish.sh
+++ publish/publish.sh
@@ -114,4 +114,4 @@ popd
 cd ${TOP}/dist
 _run s3cmd -c ${HOME}/.s3cfg.mycroft-artifact-writer sync --acl-public . s3://bootstrap.mycroft.ai/artifacts/${ARCH}/mimic/${VERSION}/
 echo ${VERSION} > ${TOP}/dist/latest
-_run s3cmd -c ${HOME}/.s3cfg.mycroft-artifact-writer put --acl-public ${TOP}/dist/latest s3://bootstrap.mycroft.ai/artifacts/${ARCH}/mimic/latest
+_run s3cmd -c ${HOME}/.s3cfg.mycroft-artifact-writer put --acl-public ${TOP}/dist/latest s3://bootstrap.mycroft.ai/artifacts/${ARCH}/mimic/latest/
